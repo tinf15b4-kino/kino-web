1e6d725314ff806369ee5a2b17cf4f28e57498ed
Jonas Kümmerlin <rgcjonas@gmail.com>
2016-10-08 23:37:51 +0200

    Replace Tomcat with Jetty to utilize inherited sockets from xinetd/systemd

--- src/main/java/de/tinf15b4/kino24/web/Kino24WebApplication.java
+++ src/main/java/de/tinf15b4/kino24/web/Kino24WebApplication.java
@@ -1,10 +1,35 @@
 package de.tinf15b4.kino24.web;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyServerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Kino24WebApplication {
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		JettyEmbeddedServletContainerFactory jetty = new JettyEmbeddedServletContainerFactory();
+		jetty.addServerCustomizers((JettyServerCustomizer) server -> jettyEnableInherit(server));
+
+		return jetty;
+	}
+
+	private static void jettyEnableInherit(Server server) {
+		for (Connector c: server.getConnectors()) {
+			if (c instanceof  ServerConnector) {
+				((ServerConnector)c).setInheritChannel(true);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Kino24WebApplication.class, args);
 	}
