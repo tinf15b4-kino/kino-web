a813c4f0063843cce899342129b70c19aa7610d4
Jonas Kümmerlin <rgcjonas@gmail.com>
2017-04-06 09:37:09 +0200

    TESB416-195: Fix double result bug
    We now temporarily save the results in a Set to get rid of duplicates

--- web app/src/main/java/de/tinf15b4/kino/data/cinemas/Cinema.java
+++ web app/src/main/java/de/tinf15b4/kino/data/cinemas/Cinema.java
@@ -23,6 +23,21 @@ public class Cinema {
     @Column(length = 100000)
     private byte[] image;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cinema)) return false;
+
+        Cinema cinema = (Cinema) o;
+
+        return id == cinema.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
     public Cinema() {
 
     }
