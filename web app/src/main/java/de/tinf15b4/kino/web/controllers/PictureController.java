28b9580c3041cb9291bc1972392b6f345a68c72d
Marco Herglotz <herglotz.m@gmail.com>
2017-04-19 12:23:39 +0200

    TESB416-204 picture controller uses default image if none was found

--- web app/src/main/java/de/tinf15b4/kino/web/controllers/PictureController.java
+++ web app/src/main/java/de/tinf15b4/kino/web/controllers/PictureController.java
@@ -1,5 +1,11 @@
 package de.tinf15b4.kino.web.controllers;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -20,11 +26,16 @@ public class PictureController {
     @Autowired
     private MovieRepository movieRepo;
 
+    private byte[] defaultCinemaImage;
+    private byte[] defaultMovieCover;
+
     @RequestMapping(value = "/picture/cinema/{cid}", produces = MediaType.IMAGE_JPEG_VALUE)
     @ResponseBody
     byte[] getCinemaPicture(@PathVariable("cid") long cinemaid) {
         Cinema c = cineRepo.findOne(cinemaid);
         if (c != null) {
+            if (c.getImage() == null)
+                return getDefaultCinemaImage();
             return c.getImage();
         } else {
             return null;
@@ -36,10 +47,24 @@ public class PictureController {
     byte[] getMoviePicture(@PathVariable("mid") long movieid) {
         Movie m = movieRepo.findOne(movieid);
         if (m != null) {
+            if (m.getCover() == null)
+                return getDefaultMovieCover();
             return m.getCover();
         } else {
             return null;
         }
+    }
+
+    private byte[] getDefaultCinemaImage() {
+        if (defaultCinemaImage == null)
+            loadDefaultCinemaImage();
+        return defaultCinemaImage;
+    }
+
+    private byte[] getDefaultMovieCover() {
+        if (defaultMovieCover == null)
+            loadDefaultMovieCover();
+        return defaultMovieCover;
     }
 
     public static String getCinemaPictureUrl(Cinema c) {
@@ -48,5 +73,36 @@ public class PictureController {
 
     public static String getMoviePictureUrl(Movie m) {
         return "/picture/movie/" + m.getId();
+    }
+
+    private void loadDefaultCinemaImage() {
+        try {
+            BufferedImage originalImage = ImageIO
+                    .read(this.getClass().getResourceAsStream("/images/defaultCinema.jpg"));
+
+            // convert BufferedImage to byte array
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(originalImage, "jpg", baos);
+            baos.flush();
+            defaultCinemaImage = baos.toByteArray();
+            baos.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void loadDefaultMovieCover() {
+        try {
+            BufferedImage originalImage = ImageIO.read(this.getClass().getResourceAsStream("/images/defaultMovie.jpg"));
+
+            // convert BufferedImage to byte array
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(originalImage, "jpg", baos);
+            baos.flush();
+            defaultMovieCover = baos.toByteArray();
+            baos.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
