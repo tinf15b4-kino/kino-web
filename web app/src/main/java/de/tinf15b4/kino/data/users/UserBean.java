c964db9836bf883e70d83467bc2b72c04be7d573
Jonas Kümmerlin <rgcjonas@gmail.com>
2017-05-01 22:53:40 +0200

    TESB416-249: Respect SMARTCINEMA_API_URL env variable during login

--- web app/src/main/java/de/tinf15b4/kino/data/users/UserBean.java
+++ web app/src/main/java/de/tinf15b4/kino/data/users/UserBean.java
@@ -22,14 +22,16 @@ public class UserBean {
     @Value("${local.server.port}")
     private int port = 0;
 
+    private String restApiUrl;
+
     @PostConstruct
     public void init() {
-        String baseUrl = System.getenv("SMARTCINEMA_API_URL");
+        restApiUrl = System.getenv("SMARTCINEMA_API_URL");
 
-        if (baseUrl == null)
-            baseUrl = "http://localhost:" + port;
+        if (restApiUrl == null)
+            restApiUrl = "http://localhost:" + port;
 
-        restClient = new RestClient(baseUrl);
+        restClient = new RestClient(restApiUrl);
     }
 
     public boolean isUserLoggedIn() {
@@ -37,7 +39,7 @@ public class UserBean {
     }
 
     public boolean login(String nameOrMail, String password) {
-        restClient = new RestClient(nameOrMail, password, "http://localhost:" + port);
+        restClient = new RestClient(nameOrMail, password, restApiUrl);
         RestResponse loginResponse = restClient.authorize();
         if (!loginResponse.hasError()) {
             // login was successful
