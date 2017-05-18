120800fdc6e1fd4133d137b61f6f4a6879393863
W-i-Z-o <knabsche@web.de>
2017-05-18 15:03:54 +0200

    TESB416-270: local state

---
+++ commons/src/test/java/de/tinf15b4/kino/TestDummy.java
@@ -0,0 +1,14 @@
+package de.tinf15b4.kino;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestDummy {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
