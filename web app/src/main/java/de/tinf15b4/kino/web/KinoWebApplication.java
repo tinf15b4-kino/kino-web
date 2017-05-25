8755a4c35374da90130a60b7b78ffa7821eefa7c
Jonas Kümmerlin <rgcjonas@gmail.com>
2017-05-26 01:41:28 +0200

    TESB416-288: Proxy publy rest API in the web app
    We need this in case the API is firewalled and no proper reverse
    proxy has been set up, as can happen when demoing the app in a
    docker container. On a similar note, we cannot let the browser
    link to the internal API url because it may be firewalled.

--- web app/src/main/java/de/tinf15b4/kino/web/KinoWebApplication.java
+++ web app/src/main/java/de/tinf15b4/kino/web/KinoWebApplication.java
@@ -3,14 +3,18 @@ package de.tinf15b4.kino.web;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.mitre.dsmiley.httpproxy.ProxyServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import de.tinf15b4.kino.web.rest.RestApiUrlSource;
 
 @SpringBootApplication
 @Configuration
@@ -23,6 +27,16 @@ public class KinoWebApplication {
         JettyEmbeddedServletContainerFactory jetty = new JettyEmbeddedServletContainerFactory();
         jetty.addServerCustomizers(KinoWebApplication::jettyEnableInherit);
         return jetty;
+    }
+
+    @Bean
+    public ServletRegistrationBean publicRestApiProxy(RestApiUrlSource apiUrl) {
+        // We proxy the public rest API here in case we are firewalled and no proper reverse proxy has been set up
+        // A proper reverse proxy will map the rest APIs entrypoints below /rest on the web app, exactly as we're doing here.
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new ProxyServlet(), "/rest/*");
+        servletRegistrationBean.addInitParameter("targetUri", apiUrl.getUrl() + "/rest");
+        servletRegistrationBean.addInitParameter(ProxyServlet.P_LOG, "false");
+        return servletRegistrationBean;
     }
 
     private static void jettyEnableInherit(Server server) {
